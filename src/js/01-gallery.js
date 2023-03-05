37969d07cff038e667538d1a12f8a872e82c7009
hannakaczynska <hannakacz13@gmail.com>
2023-03-05 11:08:36 +0100

    added js 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const galleryGrid = galleryItems
+  .map(
+    item =>
+      `<div class="gallery__item">
+      <a class="gallery__link" href=${item.original}>
+      <img class="gallery__image" src=${item.preview} data-source=${item.original} alt=${item.description}/>
+      </a>
+      </div>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryGrid);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: '250ms',
+});
