0482a0c0eea7453aa6303dcbbba8749477bcbf09
hannakaczynska <hannakacz13@gmail.com>
2023-03-05 19:28:11 +0100

    added

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+const formValue = {};
+
+form.addEventListener('input', throttle(setFormValue, 500));
+form.addEventListener('submit', submitFormValue);
+
+function setFormValue(e) {
+  formValue[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formValue));
+  console.log(formValue);
+}
+
+function submitFormValue(e) {
+  e.preventDefault();
+  const {
+    elements: { email, message },
+  } = e.target;
+  if (email.value === '' || message.value === '') {
+    alert('All fields must be filled with data');
+  } else {
+    const data = {
+      email: email.value,
+      message: message.value,
+    };
+    console.log(data);
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+  }
+}
